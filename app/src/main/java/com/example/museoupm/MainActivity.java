58c502bebcfe9fb03fa14a857e2c7abff3ed0f29
Pablo Delgado <pableqe98@gmail.com>
2021-10-13 20:43:54 +0200

    Optimización carga de preguntas. Mostrar al menos la primera pregunta en la actividad correspondiente.

--- app/src/main/java/com/example/museoupm/MainActivity.java
+++ app/src/main/java/com/example/museoupm/MainActivity.java
@@ -17,13 +17,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String GENERACION_1 = "generacion1";
+    private static final String GENERACION_2 = "generacion2";
+    private static final String GENERACION_3 = "generacion3";
+    private static final String GENERACION_4 = "generacion4";
+    private static final String GENERACION_5 = "generacion5";
+
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,172 +39,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void generacion1(View v){
-        DatabaseReference ref = database.getReference("preguntas/generacion1");
-
-        // Attach a listener to read the data at our posts reference
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                ArrayList<Pregunta> preguntas = new ArrayList<>();
-
-                HashMap respuesta =(HashMap) dataSnapshot.getValue();
-
-                Pregunta pregunta1 = new Pregunta(((HashMap)respuesta.get("pregunta1")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta1")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta1);
-
-                Pregunta pregunta2 = new Pregunta(((HashMap)respuesta.get("pregunta2")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta2")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta2);
-
-                Pregunta pregunta3 = new Pregunta(((HashMap)respuesta.get("pregunta3")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta3")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta3")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta3")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta3")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta3")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta3);
-
-                Pregunta pregunta4 = new Pregunta(((HashMap)respuesta.get("pregunta4")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta4")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta4);
-
-                Pregunta pregunta5 = new Pregunta(((HashMap)respuesta.get("pregunta5")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta5")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta5);
-
-                Generacion generacion = new Generacion(preguntas);
-
-                Intent intent = new Intent(MainActivity.this, PreguntaActivity.class);
-                intent.putExtra("GENERACION", (Parcelable) generacion);
-                startActivity(intent);
-                //System.out.println("HOLAAA "+respuesta.get("pregunta1").getClass().getName());
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
-
+        generacion("generacion1");
     }
 
     public void generacion2(View v){
-        DatabaseReference ref = database.getReference("preguntas/generacion2");
-
-        // Attach a listener to read the data at our posts reference
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                ArrayList<Pregunta> preguntas = new ArrayList<>();
-
-                HashMap respuesta =(HashMap) dataSnapshot.getValue();
-
-                Pregunta pregunta1 = new Pregunta(((HashMap)respuesta.get("pregunta1")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta1")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta1);
-
-                Pregunta pregunta2 = new Pregunta(((HashMap)respuesta.get("pregunta2")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta2")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta2);
-
-                Pregunta pregunta3 = new Pregunta(((HashMap)respuesta.get("pregunta3")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta3")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta3")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta3")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta3")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta3")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta3);
-
-                Pregunta pregunta4 = new Pregunta(((HashMap)respuesta.get("pregunta4")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta4")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta4);
-
-                Pregunta pregunta5 = new Pregunta(((HashMap)respuesta.get("pregunta5")).get("correcta").toString(),
-                        ((HashMap)respuesta.get("pregunta5")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta4").toString();
-                        }}
-                );
-                preguntas.add(pregunta5);
-
-                Generacion generacion = new Generacion(preguntas);
-
-                Intent intent = new Intent(MainActivity.this, PreguntaActivity.class);
-                intent.putExtra("GENERACION", (Parcelable) generacion);
-                startActivity(intent);
-                //System.out.println("HOLAAA "+respuesta.get("pregunta1").getClass().getName());
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                System.out.println("The read failed: " + databaseError.getCode());
-            }
-        });
+        generacion("generacion2");
     }
 
     public void generacion3(View v){
-        DatabaseReference ref = database.getReference("preguntas/generacion3");
+        generacion("generacion3");
+    }
+
+    public void generacion(String nombreGeneracion){
+        DatabaseReference ref = database.getReference("preguntas/" + nombreGeneracion );
 
         // Attach a listener to read the data at our posts reference
         ref.addValueEventListener(new ValueEventListener() {
@@ -209,28 +61,38 @@ public class MainActivity extends AppCompatActivity {
 
                 HashMap respuesta =(HashMap) dataSnapshot.getValue();
 
+                ArrayList<String> respuestas1 = new ArrayList<>(Arrays.asList(
+                        ((HashMap) respuesta.get("pregunta1")).get("respuesta1").toString(),
+                        ((HashMap) respuesta.get("pregunta1")).get("respuesta2").toString(),
+                        ((HashMap) respuesta.get("pregunta1")).get("respuesta3").toString(),
+                        ((HashMap) respuesta.get("pregunta1")).get("respuesta4").toString())
+                );
                 Pregunta pregunta1 = new Pregunta(((HashMap)respuesta.get("pregunta1")).get("correcta").toString(),
                         ((HashMap)respuesta.get("pregunta1")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta1")).get("respuesta4").toString();
-                        }}
+                        respuestas1
                 );
+
                 preguntas.add(pregunta1);
 
+                ArrayList<String> respuestas2 = new ArrayList<>(Arrays.asList(
+                        ((HashMap) respuesta.get("pregunta2")).get("respuesta1").toString(),
+                        ((HashMap) respuesta.get("pregunta2")).get("respuesta2").toString(),
+                        ((HashMap) respuesta.get("pregunta2")).get("respuesta3").toString(),
+                        ((HashMap) respuesta.get("pregunta2")).get("respuesta4").toString())
+                );
                 Pregunta pregunta2 = new Pregunta(((HashMap)respuesta.get("pregunta2")).get("correcta").toString(),
                         ((HashMap)respuesta.get("pregunta2")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta2")).get("respuesta4").toString();
-                        }}
+                        respuestas2
                 );
                 preguntas.add(pregunta2);
 
+
+                ArrayList<String> respuestas3 = new ArrayList<>(Arrays.asList(
+                        ((HashMap) respuesta.get("pregunta3")).get("respuesta1").toString(),
+                        ((HashMap) respuesta.get("pregunta3")).get("respuesta2").toString(),
+                        ((HashMap) respuesta.get("pregunta3")).get("respuesta3").toString(),
+                        ((HashMap) respuesta.get("pregunta3")).get("respuesta4").toString())
+                );
                 Pregunta pregunta3 = new Pregunta(((HashMap)respuesta.get("pregunta3")).get("correcta").toString(),
                         ((HashMap)respuesta.get("pregunta3")).get("enunciado").toString(),
                         new ArrayList<String>(){{
@@ -242,34 +104,39 @@ public class MainActivity extends AppCompatActivity {
                 );
                 preguntas.add(pregunta3);
 
+
+                ArrayList<String> respuestas4 = new ArrayList<>(Arrays.asList(
+                        ((HashMap) respuesta.get("pregunta4")).get("respuesta1").toString(),
+                        ((HashMap) respuesta.get("pregunta4")).get("respuesta2").toString(),
+                        ((HashMap) respuesta.get("pregunta4")).get("respuesta3").toString(),
+                        ((HashMap) respuesta.get("pregunta4")).get("respuesta4").toString())
+                );
                 Pregunta pregunta4 = new Pregunta(((HashMap)respuesta.get("pregunta4")).get("correcta").toString(),
                         ((HashMap)respuesta.get("pregunta4")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta4")).get("respuesta4").toString();
-                        }}
+                        respuestas4
                 );
                 preguntas.add(pregunta4);
 
+
+                ArrayList<String> respuestas5 = new ArrayList<>(Arrays.asList(
+                        ((HashMap) respuesta.get("pregunta5")).get("respuesta1").toString(),
+                        ((HashMap) respuesta.get("pregunta5")).get("respuesta2").toString(),
+                        ((HashMap) respuesta.get("pregunta5")).get("respuesta3").toString(),
+                        ((HashMap) respuesta.get("pregunta5")).get("respuesta4").toString())
+                );
                 Pregunta pregunta5 = new Pregunta(((HashMap)respuesta.get("pregunta5")).get("correcta").toString(),
                         ((HashMap)respuesta.get("pregunta5")).get("enunciado").toString(),
-                        new ArrayList<String>(){{
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta1").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta2").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta3").toString();
-                            ((HashMap) respuesta.get("pregunta5")).get("respuesta4").toString();
-                        }}
+                        respuestas5
                 );
                 preguntas.add(pregunta5);
 
                 Generacion generacion = new Generacion(preguntas);
 
+
                 Intent intent = new Intent(MainActivity.this, PreguntaActivity.class);
                 intent.putExtra("GENERACION", (Parcelable) generacion);
                 startActivity(intent);
-                //System.out.println("HOLAAA "+respuesta.get("pregunta1").getClass().getName());
+
 
             }
 
--- app/src/main/java/com/example/museoupm/PreguntaActivity.java
+++ app/src/main/java/com/example/museoupm/PreguntaActivity.java
@@ -4,23 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class PreguntaActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pregunta);
 
+        TextView enunciado = (TextView) findViewById(R.id.txtPregunta);
+        RadioButton respuesta1 = (RadioButton) findViewById(R.id.respuesta1);
+        RadioButton respuesta2 = (RadioButton) findViewById(R.id.respuesta2);
+        RadioButton respuesta3 = (RadioButton) findViewById(R.id.respuesta3);
+        RadioButton respuesta4 = (RadioButton) findViewById(R.id.respuesta4);
+
+
         Intent intent = getIntent();
         Generacion generacion = intent.getExtras().getParcelable("GENERACION");
 
         ArrayList<Pregunta> preguntas = generacion.getPreguntas();
 
+        Pregunta pregunta1 = preguntas.get(0);
+        ArrayList<String> respuestas = pregunta1.getRespuestas();
+        System.out.println(respuestas.toString());
+
+        enunciado.setText(pregunta1.getEnunciado());
+        respuesta1.setText(respuestas.get(0));
+        respuesta2.setText(respuestas.get(1));
+        respuesta3.setText(respuestas.get(2));
+        respuesta4.setText(respuestas.get(3));
+
         //for (Pregunta pregunta : preguntas){
-            System.out.println(preguntas.size());
+        System.out.println(preguntas.size());
         //}
 
 
