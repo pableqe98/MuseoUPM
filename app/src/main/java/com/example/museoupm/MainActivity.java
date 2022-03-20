779aad5f705e638162706b55c55ac5a3492e662b
pableqe98 <pableqe98@gmail.com>
2022-03-20 17:59:49 +0100

    Realizado cambio a utilización de menu inferior.

--- app/src/main/java/com/example/museoupm/MainActivity.java
+++ app/src/main/java/com/example/museoupm/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.museoupm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +13,9 @@ import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 
+import com.example.museoupm.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,6 +32,8 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     private static final String GENERACION_1 = "generacion1";
     private static final String GENERACION_2 = "generacion2";
     private static final String GENERACION_3 = "generacion3";
@@ -37,9 +45,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         String email = "";
         String tipo = "google";
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.home);
 
         if (getIntent().hasExtra("email")) {
             Bundle content = getIntent().getExtras();
@@ -54,6 +66,30 @@ public class MainActivity extends AppCompatActivity {
             initializeDBinfo(email);
         }
 
+        replaceLayout(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceLayout(new HomeFragment());
+                    break;
+                case R.id.qr:
+                    replaceLayout(new QrFragment());
+                    break;
+                case R.id.settings:
+                    replaceLayout(new SettingsFragment());
+                    break;
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceLayout(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 
     public void initializeDBinfo(String email){
